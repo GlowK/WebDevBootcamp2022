6bfd0089303a6c8dcaa5095ba087fc77033a19bd
GlowK <glow.k@poczta.fm>
2022-03-21 20:08:21 +0100

    YelpCamp Basic Styling

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -13,7 +13,7 @@ db.once("open", () => {
 });
 
 // const sample = (array) => array[Math.floor(Math.random() * array.length)];
-const sample = function(array){
+const sample = function (array) {
     return array[Math.floor(Math.random() * array.length)]
 };
 
@@ -21,12 +21,16 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const random20 = Math.floor(Math.random() * 20) + 2;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque assumenda nulla odio sed ipsam aut totam cupiditate voluptas. Quasi possimus architecto sit at quae atque, error doloremque reprehenderit dolores consequuntur?",
+            price: random20
         })
         await camp.save();
-        
+
     }
     console.log("Baza danych zaktualizowana o randomowe dane")
 }
