6c5bbf77567cbb41be811967324c2ad43206ee9e
GlowK <glow.k@poczta.fm>
2022-04-03 14:38:38 +0200

    Yelp_Comments

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -6,7 +7,25 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
+
+
+//Jak usunac wszystkie review podczas usuwania campgroundu - QueryMiddleware
+//Middleware - funkcha odpalana po FindOneAndDelete 
+//https://mongoosejs.com/docs/middleware.html
+//https://mongoosejs.com/docs/api.html#query_Query-findOneAndDelete
+CampgroundSchema.post('findOneAndDelete', async function (campground) {
+    if (campground.reviews.length) {
+        const res = await Review.deleteMany({ _id: { $in: campground.reviews } })
+        console.log(res);
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
